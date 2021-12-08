b6f2dacf7e49316935db0d748ccc281fe0369795
Amihaeseisergiu <61450080+Amihaeseisergiu@users.noreply.github.com>
2021-12-08 15:57:16 +0200

    New accuracy 89.96%

--- homework5/hw5.py
+++ homework5/hw5.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[300]:
+# In[36]:
 
 
 import numpy as np
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.layers import Dense, LSTM, Embedding, Input
-from tensorflow.keras.layers import Dropout, Conv1D
+from tensorflow.keras.layers import Dropout, Conv1D, Flatten, AveragePooling1D
 from tensorflow.keras.layers import Activation, BatchNormalization, Add
 from tensorflow.keras.callbacks import LearningRateScheduler
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.models import Model
 
 
-# In[301]:
+# In[37]:
 
 
 max_words = 5000
@@ -25,10 +24,9 @@ embedding_vecor_length = 32
 
 epochs = 3
 batch_size = 64
-learning_rate = 0.1
 
 
-# In[302]:
+# In[38]:
 
 
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_words)
@@ -37,7 +35,7 @@ x_train = sequence.pad_sequences(x_train, maxlen=max_review_length)
 x_test = sequence.pad_sequences(x_test, maxlen=max_review_length)
 
 
-# In[303]:
+# In[39]:
 
 
 inputs = Input(shape=(None,), dtype="int32")
@@ -50,7 +48,10 @@ x = Activation('elu')(x)
 x = Conv1D(filters=32, kernel_size=3, padding='same')(x)
 x = BatchNormalization()(x)
 outputs = Add()([shortcut, x])
+outputs = Activation('elu')(outputs)
+outputs = AveragePooling1D()(outputs)
 outputs = LSTM(125)(outputs)
+outputs = Flatten()(outputs)
 outputs = Dropout(0.2)(outputs)
 outputs = Dense(1, activation='sigmoid')(outputs)
 model = Model(inputs=inputs, outputs=outputs)
@@ -60,7 +61,7 @@ model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']
 model.summary()
 
 
-# In[304]:
+# In[40]:
 
 
 model.fit(x_train, y_train, validation_data=(x_test, y_test),
